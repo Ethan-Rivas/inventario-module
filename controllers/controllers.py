5febb3dfcb13eb9f093d7a860fb6ca30564fc460
Ethan-Rivas <ethan-rivas@hotmail.com>
2017-03-10 17:45:38 -0600

    endpoint agregado

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,10 +1,41 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo import models
 
-# class Inventario(http.Controller):
-#     @http.route('/inventario/inventario/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Inventario(http.Controller):
+    @http.route('/api/inventario/', auth='user', type='json', methods=['POST'], csrf=False)
+    def index(self, telegram_chat_id, codigo_de_producto, cantidad_contada):
+
+        partners = http.request.env['res.partner']
+        partner = partners.search([('telegram_chat_id','=', telegram_chat_id)])
+
+        inventarios = http.request.env['inventario.inventario']
+        inventario = inventarios.search([('partner', '=', partner.id)])
+
+        linea_inventarios = http.request.env['linea.inventario']
+        linea_inventario = linea_inventarios.search([('codigo', '=', codigo_de_producto)])
+
+
+        if len(inventario) >= 1:
+            linea_inventario.cantidad_contada = cantidad_contada
+
+            return {
+                        "response": "OK",
+                        "inventario_partner": len(inventario),
+                        "data": {
+                                    "partner": partner.id,
+                                    "codigo": linea_inventario.codigo
+                                }
+                    }
+        else:
+            return {
+                        "response": "NO",
+                        "inventario_partner": len(inventario),
+                        "data": {
+                                    "partner": partner.id,
+                                    "codigo": linea_inventario.codigo
+                                }
+                    }
 
 #     @http.route('/inventario/inventario/objects/', auth='public')
 #     def list(self, **kw):
